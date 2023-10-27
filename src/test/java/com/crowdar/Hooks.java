e599e9463786043b0e9f3e98c07612b0f408a402
Mariana_Cervantes <cervantes.mariana.6017@gmail.com>
2023-10-27 19:43:55 -0300

    Correccion en hook

--- src/test/java/com/crowdar/Hooks.java
+++ src/test/java/com/crowdar/Hooks.java
@@ -1,15 +1,19 @@
 package com.crowdar;
 
+
 import com.crowdar.core.actions.MobileActionManager;
 import com.crowdar.examples.constants.HomeConstants;
+import io.cucumber.core.api.Scenario;
 import io.cucumber.java.After;
 
 
 public class Hooks {
     @After(order = Integer.MAX_VALUE)
-    public void afterScenario() {
-        MobileActionManager.click(HomeConstants.BUTTON_MENU_ENTRY);
-        MobileActionManager.click(HomeConstants.BUTTON_DELETE_ENTRY);
-        MobileActionManager.waitVisibility(HomeConstants.NO_ENTRY);
+    public void afterScenario(Scenario scenario) {
+        if(scenario.getName().equals("Add entry data")) {
+            MobileActionManager.click(HomeConstants.BUTTON_MENU_ENTRY);
+            MobileActionManager.click(HomeConstants.BUTTON_DELETE_ENTRY);
+            MobileActionManager.waitVisibility(HomeConstants.NO_ENTRY);
+        }
     }
 }
